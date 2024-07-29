72ee85b59a8255f1f5995f2dc09c4d4566581103
portal222 <dvaportala@gmail.com>
2024-07-29 15:21:19 +0200

    new commit

---
+++ src/components/result/PaginationCM.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PaginationCM = (data, pageSize) => {
+
+
+
+    const pageCount = Math.ceil(data.length / pageSize);
+    return Array.from({ length: pageCount}, (_, index) =>
+data.slice(index * pageSize, (index + 1) * pageSize)
+);
+};
+export default PaginationCM;
